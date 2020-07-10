7fd755f0e72e94b6938b7f0b37f068c6f7f5e0ad
Charles Roth <charlieroth4@icloud.com>
2020-07-10 08:17:23 -0400

    Organization of functions

--- todo_cache/lib/todo/database.ex
+++ todo_cache/lib/todo/database.ex
@@ -1,5 +1,6 @@
 defmodule Todo.Database do
   use GenServer
+  alias Todo.DatabaseWorker
 
   @db_folder "./persist"
 
@@ -7,39 +8,52 @@ defmodule Todo.Database do
     GenServer.start(__MODULE__, nil, name: __MODULE__)
   end
 
+  @doc """
+  Obtain a worker's pid and forward to DatabaseWorker.store/3
+  """
   def store(key, data) do
-    GenServer.cast(__MODULE__, {:store, key, data})
+    key
+    |> choose_worker()
+    |> Todo.DatabaseWorker.store(key, data)
   end
-
+  
+  @doc """
+  Obtain a worker's pid and forward to DatabaseWorker.get/2
+  """
   def get(key) do
-    GenServer.call(__MODULE__, {:get, key})
+    key
+    |> choose_worker()
+    |> Todo.DatabaseWorker.get(key)
+  end
+  
+  defp choose_worker(key) do
+    GenServer.call(__MODULE__, {:choose_worker, key}) 
   end
 
+  @doc """
+  Creates directory to store data in
+  """
   @impl GenServer
   def init(_) do
     File.mkdir_p!(@db_folder)
-    {:ok, nil}
+    {:ok, start_workers()}
   end
-
+  
+  defp start_workers() do
+    # Spawns three workers for the Database to delgate worker to
+    for index <- 1..3, into: %{} do
+      {:ok, pid} = DatabaseWorker.start(@db_folder)
+      {index - 1, pid}
+    end
+  end
+  
+  @doc """
+  :choose_worker should always return the same worker for the same key
+  Compute the key's numerical hash and normalixe it to fall in the range [0,2]
+  """
   @impl GenServer
-  def handle_cast({:store, key, data}, state) do
-    file_path = file_name(key)
-    File.write!(file_path, :erlang.term_to_binary(data))
-    {:noreply, state}
-  end
-
-  @impl GenServer
-  def handle_call({:get, key}, _, state) do
-    data = 
-      case File.read(file_name(key)) do
-        {:ok, contents} -> :erlang.binary_to_term(contents)
-        _ -> nil
-      end
-
-    {:reply, data, state}
-  end
-
-  defp file_name(key) do
-    Path.join(@db_folder, to_string(key))
+  def handle_call({:choose_worker, key}, _, workers) do
+    worker_key = :erlang.phash2(key, 3)
+    {:reply, Map.get(workers, worker_key), workers}
   end
 end
